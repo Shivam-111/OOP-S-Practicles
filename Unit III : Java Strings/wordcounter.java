a9d2ce5082784622295943bcd1ba3ee63dd0dad8
Shivam Dipte <shivamdipte0@gmail.com>
2026-03-28 22:15:04 +0530

    Problem no 6 from unit 3
    // 6.	Create a class WordCounter. 
    //  Accept a sentence from the user.
     //  Use StringTokenizer to count words.
     // Display total number of words.

---
+++ Unit III : Java Strings/wordcounter.java
@@ -0,0 +1,18 @@
+// 6.	Create a class WordCounter.
+//  Accept a sentence from the user. 
+//  Use StringTokenizer to count words. 
+// Display total number of words. 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+class wordcounter{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Sentence: ");
+        String sentence = sc.nextLine();
+
+        StringTokenizer tokenizer = new StringTokenizer(sentence);
+        int WordCount = tokenizer.countTokens();
+        System.out.println("Total Count Words: " + WordCount);
+        sc.close();
+    }
+}
